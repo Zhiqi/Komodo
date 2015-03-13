410c8c0705cc70197cee3c2f330918a40be60dd3
Boren Zhang <bobo1993324@gmail.com>
2015-03-13 00:13:28 -0400

    check touch

--- Cooties/app/src/main/java/edu/umich/eecs/cooties/CheckTouch.java
+++ Cooties/app/src/main/java/edu/umich/eecs/cooties/CheckTouch.java
@@ -3,7 +3,56 @@ package edu.umich.eecs.cooties;
 /**
  * Created by Zhiqi on 3/12/2015.
  */
-public class CheckTouch {
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class CheckTouch {
+    Hashtable<Long, ArrayList> dict;
+
+    public CheckTouch(){
+        dict = new Hashtable<Long, ArrayList>();
+    }
+
+    ArrayList bumpEvents(TouchMessage msg) {
+        final int timeSpanAllowed = 3;
+
+        ArrayList<TouchMessage> found = new ArrayList<TouchMessage>();
+
+        // Find valid touch message
+        for(int i = -timeSpanAllowed; i < timeSpanAllowed + 1; ++i) {
+            long num = msg.timestamp + i;
+            ArrayList<TouchMessage> search = dict.get(num);
+            if(search != null) {
+                //test if any collected touches are in the current touch and vice versa
+                for(TouchMessage touch : search) {
+                    if(msg.targetUserId == touch.sourceUserId && touch.targetUserId == msg.sourceUserId) {
+                        found.add(touch);
+                    }
+                }
+            }
+        }
+
+        addTouch(msg);
+
+        // Remove duplicate
+        if(found.size() > 0) {
+            for (int i = found.size() - 1; i > 0; i--){
+                if (found.indexOf(found.get(i)) < i){
+                    found.remove(i);
+                }
+            }
+        }
+        return found;
+    }
+
+    void addTouch(TouchMessage msg){
+        ArrayList<TouchMessage> array = dict.get(msg.timestamp);
+        if(array == null) {
+            array = new ArrayList();
+            dict.put(msg.timestamp, array);
+        }
+        array.add(msg);
+    }
 
 }
