454d4f863f149d6933c611d12c7ae948ebbac96f
mtkliema <mtkliema@umich.edu>
2015-03-20 12:17:09 -0400

    shouldn't limit to your min, use null for minor

--- Cooties/app/src/main/java/edu/umich/eecs/cooties/StudentPlayActivity.java
+++ Cooties/app/src/main/java/edu/umich/eecs/cooties/StudentPlayActivity.java
@@ -171,7 +171,7 @@ public class StudentPlayActivity extends Activity implements BeaconConsumer, Col
             Log.d("SPA", "maj is "+maj.toString());
             Log.d("SPA", "min is "+min.toString());
 
-            beaconManager.startRangingBeaconsInRegion(new Region("myApplicationRanger", uuid, maj, min));
+            beaconManager.startRangingBeaconsInRegion(new Region("myApplicationRanger", uuid, maj, null));
 //                    new Region("myRangingUniqueI",null),null,null)
         } catch (RemoteException e) {
         Log.e("SPA", "Beacon Service Connect Start Ranging error", e);
